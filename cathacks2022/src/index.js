81688e29db9ca60e0cb6deb53ba6869b03c2cff5
eptate01 <ethanptate@gmail.com>
2022-04-10 09:55:26 -0400

    Added example events

--- cathacks2022/src/index.js
+++ cathacks2022/src/index.js
@@ -4,27 +4,37 @@ import App from "./App"
 import EventCard from './EventCard'
 
 class Event {
-    constructor(id, name, location, date, host) {
+    constructor(id, name, location, date, host, url) {
       this.id = id;
       this.name = name;
       this.location = location;
       this.date = date;
       this.host = host;
+      this.url = url;
   
     }
 }
-    function AddEvent(id, name, location, date, host) {
-        events.push(new Event(id, name, location, date, host));
+    function AddEvent(id, name, location, date, host, url) {
+        events.push(new Event(id, name, location, date, host, url));
       }
 let events = []
-AddEvent(0, "Cat Hacks VII", "Grehan First Floor", "April 9th, 2022", "UK");
-AddEvent(1, "Cat Hacks VI", "Grehan Second Floor", "April 10th, 2022", "UK ACM");
-AddEvent(2, "Cat Hacks V", "Grehan Third Floor", "April 11th, 2022", "UK-ACM");
-AddEvent(3, "Cat Hacks IX", "Grehan Fourth Floor", "April 12th, 2022", "UK+ACM");
-AddEvent(4, "Cat Hacks X", "Grehan Fifth Floor", "April 13th, 2022", "UK=ACM");
-AddEvent(5, "Cat Hacks VIII", "Grehan Sixth Floor", "April 14th, 2022", "UK--ACM");
+AddEvent(0, "Cat Hacks VII", "Grehan First Floor", "April 9th, 2022", "UK ACM","./UKFootball.jpg");
+AddEvent(1, "UGC Exec Meeting", "GSC 330C", "April 11th, 2022", "United Greek Council", "./GreekCouncil.png");
+AddEvent(2, "CPA Exam Info", "Gatton B&E", "April 11th, 2022", "KY-CPA","./cpaExam.png");
+AddEvent(3, "FREE Cookies", "Gatton Student Center", "April 11th, 2022", "Financial Wellness","./FreeCookies.png");
+AddEvent(4, "UKCS Practice", "Johnson Center", "April 12th, 2022", "UK=ACM","./Swimming.png");
+AddEvent(5, "Registration 101", "Gatton Student Center", "April 12th, 2022", "UK--ACM","./Registration.png");
 
 ReactDOM.render(<App events={events}/>, document.getElementById('root'));
+let eventCards = document.getElementsByClassName("event-card");
+events.forEach(user => {
+    console.log(document.getElementById(user.id).firstChild);
+    document.getElementById(user.id).firstChild.style.backgroundImage = "url('" + user.url + "')";
+})
+
+
+
+
 const searchInput = document.querySelector("[data-search]");
   searchInput.addEventListener("input", (e) =>{
     const value = e.target.value
@@ -33,10 +43,6 @@ const searchInput = document.querySelector("[data-search]");
     events.forEach(user => {
         const isVisible = user.name.includes(value) || user.location.includes(value) || user.date.includes(value)
         
-        // var style = '#' + user.id + ' {';
-        // style += 'display : none;';
-        // style += '}';
-        // console.log(sheet)
         if(!isVisible) {
             const sheet = document.getElementById(user.id).style.display = "none";
             // sheet.insertRule(style, 0)
@@ -50,11 +56,3 @@ const searchInput = document.querySelector("[data-search]");
         
     })
   })
-
-  /*for(let eventCard of eventCards) {
-        console.log(eventCard)
-        const isVisible = eventCard.props.name.includes(value) || eventCard.location.includes(value) || eventCard.date.includes(value)
-
-        eventCard.classList.toggle("hide", !isVisible)
-        console.log(eventCard.classList)
-    }*/
